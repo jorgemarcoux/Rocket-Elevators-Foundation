5864ac42655ea7d4c4e9fdd9eccefa7378421c01
LOÏC RICO <ricoloic4@gmail.com>
2020-10-12 17:15:19 -0400

    picture add/not all

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -10,3 +10,12 @@
 //= link extralayers.css
 //= link settings.css
 //= link bootstrap.min.css
+
+//= link contact
+//= link jquery-3.3.1.min.js
+//= link demo.revolution_slider.js
+//= link popper.min.js.map
+//= link jquery.themepunch.revolution.min
+//= link jquery.themepunch.tools.min
+//= link quote
+//= link scripts
