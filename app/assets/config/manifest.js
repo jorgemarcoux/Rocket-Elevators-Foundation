3b29909dc231fc70676cc7f7fbfffaadf5a54436
LOÏC RICO <ricoloic4@gmail.com>
2020-10-12 14:45:51 -0400

    link stylesheets : ok

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,12 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link essentials.css
+//= link layout.css
+//= link red.css
+//= link custom.css
+//= link header.css
+//= link extralayers.css
+//= link settings.css
+//= link bootstrap.min.css
