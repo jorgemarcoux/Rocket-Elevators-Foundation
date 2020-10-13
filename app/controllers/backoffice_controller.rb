492d463186011dc0b44b125329ae6ae6f01d443d
WilliamSinclairF <willzy@outlook.com>
2020-10-13 09:51:23 -0400

    DRY: added partials, fixed links in header

---
+++ app/controllers/backoffice_controller.rb
@@ -0,0 +1,5 @@
+class BackofficeController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/quote'
   get 'static_pages/residential'
   get 'static_pages/corporate'
+
+  get "backoffice", to: "backoffice#index", as: :backoffice
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
