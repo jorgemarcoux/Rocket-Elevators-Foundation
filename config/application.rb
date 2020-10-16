2bc2b8433fc2ffc055442d12a79ce91b704bcf8e
WilliamSinclairF <willzy@outlook.com>
2020-10-16 17:24:02 -0400

    trying to fix backoffice issue

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.api_only = false
 
     config.session_store :cookie_store
     config.middleware.use ActionDispatch::Cookies
