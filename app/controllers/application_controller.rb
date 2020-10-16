7dda94880c280a4fd41afe5aa98817ae1f983c92
WilliamSinclairF <willzy@outlook.com>
2020-10-16 16:27:10 -0400

    deploy attempt #????????????

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
   def after_sign_in_path_for(resource)
     if current_user.is_admin?
       rails_admin_path
