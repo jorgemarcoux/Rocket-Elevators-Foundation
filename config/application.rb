18ca5cabaef37f9d0b21bfc7470a58bab6bafe7e
WilliamSinclairF <willzy@outlook.com>
2020-10-16 17:12:02 -0400

    trying to fix backoffice issue

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.session_store :cookie_store
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use Rack::MethodOverride
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
