094cfa4abee12c928fff23fc7d4b116824a4d712
WilliamSinclairF <willzy@outlook.com>
2020-10-16 15:42:45 -0400

    deploy attempt #??

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,25 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users,
-  :controllers => { registrations: 'registrations'},
-  :path_prefix => '',
-  path: 'u',
-  path_names: {
-    sign_in: 'sign_in',
-    sign_out: 'sign_out',
-    password: 's',
-    confirmation: 'v'
-  }
+  # devise_for :users,
+  # :controllers => { registrations: 'registrations'}
+  # # :path_prefix => '',
+  # # path: 'u',
+  # # path_names: {
+  # #   sign_in: 'sign_in',
+  # #   sign_out: 'sign_out',
+  # #   password: 's',
+  # #   confirmation: 'v'
+  # # }
+
+  devise_for :users, skip: [:sessions]
+  as :user do
+  get 'signin', to: 'devise/sessions#new', as: :new_user_session
+  post 'signin', to: 'devise/sessions#create', as: :user_session
+  delete 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+
   get 'my_quotes' => 'quotes#user_quotes', as: :my_quotes
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
