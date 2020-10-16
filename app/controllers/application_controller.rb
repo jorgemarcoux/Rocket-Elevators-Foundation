cc3a31ca97e9230e8697dc0c7c6bcb8446ee27fa
WilliamSinclairF <willzy@outlook.com>
2020-10-16 15:59:56 -0400

    deploy attempt #????

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-  # protect_from_forgery prepend: true
+  protect_from_forgery prepend: true
   before_action :authenticate_user!
   # protect_from_forgery prepend: true, with: :exception
   # skip_before_filter :verify_authenticity_token, :only => :create
