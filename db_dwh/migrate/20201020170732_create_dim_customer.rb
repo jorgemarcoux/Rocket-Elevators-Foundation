c2eefa21f225c6448d4d5b8dade0c3df3b2b1e36
WilliamSinclairF <willzy@outlook.com>
2020-10-20 16:29:30 -0400

    fixed migrations

--- db_dwh/migrate/20201020170732_create_dim_customer.rb
+++ db_dwh/migrate/20201020170732_create_dim_customer.rb
@@ -2,6 +2,7 @@ class CreateDimCustomer < ActiveRecord::Migration[5.2]
   def change
     create_table :dim_customers do |t|
       
+      t.date :creation_date
       t.string :company_name #customer table
       t.string :company_contact_full_name #customer table
       t.string :company_contact_email #customer table
