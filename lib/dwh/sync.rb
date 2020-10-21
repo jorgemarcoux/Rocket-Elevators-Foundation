fe57de7fdbd04a050c9c95b65d5be9d1e0a29609
LOÏC RICO <65566752+ricoloic@users.noreply.github.com>
2020-10-21 08:42:40 -0400

    Update sync.rb

--- lib/dwh/sync.rb
+++ lib/dwh/sync.rb
@@ -2,51 +2,44 @@ module Dwh
   class Sync
     def self.sync_fact_quotes
       for q in Quote.all do
-        # customer = Customer.where("customer_id" => q.customer_id)
-        # lead = Lead.where("user_id" => user.id)
-
-        FactQuote.create({
+        user = User.find(q.user_id)
+        customer = Customer.find(user.id)
+        FactQuote.create!({
           quote_id: q.id,
           quote_created_at: q.created_at,
-          # company_name: customer.business_name,
-          # company_name: customer.company_name,
+          company_name: customer.company_name,
           email: user.email,
           elevator_number: q.elevator_number
-        })
+          })
       end
     end
 
     def self.sync_dim_customers
       for c in Customer.all do
         elevator_number = 0
-        customer_buildings = Building.where("customer_id" => c.id)
-
-        for build in customer_buildings do
-          batteries = Battery.where("building_id" => build.id)
-
-          for bat in batteries do
-            columns = Column.where("battery_id" => bat.id)
-
-            for col in columns do
-              elevators = Elevator.where("column_id" => col.id)
+        customer_buildings = Building.where(customer_id: c.id).to_a
+        for building in customer_buildings do
+          batteries = Battery.where(building_id: building.id).to_a
+          for battery in batteries do
+            columns = Column.where(battery_id: battery.id).to_a
+            for column in columns do
+              elevators = Elevator.where(column_id: column.id).to_a
               elevator_number += elevators.size
             end
           end
         end
-
-        address = Address.find(c.address_id)
-
-        DimCustomer.create({
-          created_at: c.created_at,
+        city = Address.find(c.address_id).city
+        DimCustomer.create!({
+          customer_creation_date: c.created_at,
           company_name: c.company_name,
           company_contact_full_name: c.company_contact_full_name,
           company_contact_email: c.company_contact_email,
           elevator_number: elevator_number,
-          customer_city: address.city
+          customer_city: city
         })
       end
     end
-
+    
     def self.sync_fact_leads
       for l in Lead.all do
         FactContact.create!({
@@ -56,11 +49,6 @@ module Dwh
           project_name: l.project_name,
           created_at: l.created_at
         })
-      end
-    end
-
-    def self.sync_fact_elevators
-
-    end
+     end
   end
 end
