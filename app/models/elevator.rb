154e258387c3564037e24136feeb567b12528146
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-03 15:32:16 -0500

    elevator.rb _was instead of .was

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -9,6 +9,6 @@ end
 def slack_notifier_messsage
    if self.elevator_status_changed? 
     notifier = Slack::Notifier.new Rails.application.credentials.slack[:token]
-    notifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.elevator_status.was} to #{self.elevator_status}"
+    notifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.elevator_status_was} to #{self.elevator_status}"
    end 
 end
