46b251807e202de9560e8264c8957d99e7b104e0
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-03 11:25:23 -0500

    Messages are successfuly sent to Slack after running file on terminal

---
+++ vendor/slack-notifier-message.rb
@@ -0,0 +1,6 @@
+require 'slack-notifier'
+
+notifier = Slack::Notifier.new Rails.application.credentials.slack[:token]
+
+notifier.ping "The Elevator [Elevator’s ID] with Serial Number [Serial Number] changed status from [Old Status] to [New Status]
+"
