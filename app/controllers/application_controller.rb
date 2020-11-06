c85cccd606e6ff734f3050af6b1a52d0b7512db0
WilliamSinclairF <willzy@outlook.com>
2020-11-06 15:12:13 -0500

    few more fixes to map page and added more logic to sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  include WatsonHelper
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { head :forbidden, content_type: 'text/html' }
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,29 +1,32 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  include WatsonHelper # before_action :configure_sign_in_params, only: [:create]
+  include WatsonHelper
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  #   greeting_message
-  # end
+  def new
+    super
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+  end
 
   # POST /resource/sign_in
   def create
     super
     greeting_message
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def destroy
+    super
+    signed_out =
+      (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+  end
 end
