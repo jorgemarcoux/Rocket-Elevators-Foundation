d32382930913ae46441f73d60599a031809147e8
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-18 15:13:09 -0500

    building and battery dropdown working

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,5 +1,5 @@
 class InterventionsController < ApplicationController
-   
+    respond_to :js, :json, :html
     def new
         @intervention = Intervention.new
     end
@@ -26,7 +26,35 @@ class InterventionsController < ApplicationController
         )
     end
 
-       
+    #Controllers for AJAX request
+    def get_build_request
+      customer_id = params[:customer_id]
+      buildings = Building.where(customer_id: customer_id)
+      render json: buildings
+    end
+
+    def get_batt_request
+        building_id = params[:building_id]
+        batteries = Battery.where(building_id: building_id)
+        render json: batteries
+    end
+
+    def get_col_request
+        column_id = params[:column_id]
+        columns = Column.where(battery_id: battery_id)
+        render json: columns
+    end
+
+    def get_elev_reques
+        elevator_id = params[:elevator_id]
+        elevators = Elevator.where(column_id: column_id)
+        render json: elevators
+    end
+
+
+
+    
+   
     
 
     
--- vendor/assets/javascripts/intervention_form.js
+++ vendor/assets/javascripts/intervention_form.js
@@ -9,14 +9,29 @@ $("#select_customer").change(function(){
     $("#build_label").removeClass("d-none");
     $("#select_build").removeClass("d-none");
     customerID = $(this).val();
-  });
+    //Ajax request to interventions_controller.rb
+    $.get("/interventions/get_build_request", {customer_id: customerID}, function(buildings) {
+      $("#select_build").empty();
+      //Displaying each element of building array as a dropdown option
+      buildings.forEach(building => $("#select_build").append(`<option value="${building.id}"> ${building.administrator_full_name} </option>`)); 
+    });
+
+  });//End customer dropdown listener
 
 //Showing battery dropdown once building has been selected
 $("#select_build").change(function(){
     $("#select_bat").removeClass("d-none");
     $("#bat_label").removeClass("d-none");
     buildID = $(this).val();
-  });
+    //Ajax request to interventions_controller.rb
+    $.get("/interventions/get_batt_request", {building_id: buildID}, function(batteries) {
+      $("#select_bat").empty();
+      //Displaying each element of building array as a dropdown option
+      batteries.forEach(battery => $("#select_bat").append(`<option value="${battery.id}"> ${battery.id} </option>`)); 
+    });
+
+
+  });//End battery dropdown listener
 
 //Showing column dropdown once battery has been selected
 $("#select_bat").change(function(){
