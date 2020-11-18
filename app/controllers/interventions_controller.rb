8fa8c6b764379c1921363ceb6298c6c3ba48b4d3
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-17 21:49:49 -0500

    Author id missing

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,6 +1,6 @@
 require 'zendesk_api'
 class InterventionsController < ApplicationController
-    after_action :create_intervention_ticket #Create ticket after form submition
+   # after_action :create_intervention_ticket #Create ticket after form submition
 
     def new
         @intervention = Intervention.new
@@ -25,32 +25,32 @@ class InterventionsController < ApplicationController
     end
 
     #Creating a new Zenddesk interveniton ticket
-    def create_intervention_ticket
-       client = ZendeskAPI::Client.new do |config|
-        config.url = ENV["ZENDESK_URL"]
+    # def create_intervention_ticket
+    #    client = ZendeskAPI::Client.new do |config|
+    #     config.url = ENV["ZENDESK_URL"]
 
-        config.username = ENV["ZENDESK_USERNAME"]
-        config.token = ENV["ZENDESK_TOKEN"]
-    end
+    #     config.username = ENV["ZENDESK_USERNAME"]
+    #     config.token = ENV["ZENDESK_TOKEN"]
+    # end
 
-    ZendeskAPI::Ticket.create!(
-        client, :subject => "New intervention request for client ID: #{params[:customer_id]}}", 
-        :comment => { :value => "A new intervention was requested for client with id: #{params[:customer_id]}
-        by employee ID #{params[:author]}.
+    # ZendeskAPI::Ticket.create!(
+    #     client, :subject => "New intervention request for client ID: #{params[:customer_id]}}", 
+    #     :comment => { :value => "A new intervention was requested for client with id: #{params[:customer_id]}
+    #     Requester ID: #{params[:author]}.
         
-        Necessary information for the intervention:
-        -Building ID = #{params[:building_id]}
-        -Battery ID = #{params[:battery_id]}
-        -Column ID = #{params[:column_id]}
-        -Elevator ID = #{params[:elevator_id]}
-        -Employee ID = #{params[:employee_id]}
-        -Report = #{params[:report]}" }, 
-        :priority => "normal",
-        :type => "problem"
-        )
+    #     Necessary information for the intervention:
+    #     -Building ID = #{params[:building_id]}
+    #     -Battery ID = #{params[:battery_id]}
+    #     -Column ID = #{params[:column_id]}
+    #     -Elevator ID = #{params[:elevator_id]}
+    #     -Employee ID = #{params[:employee_id]}
+    #     -Report = #{params[:report]}" }, 
+    #     :priority => "normal",
+    #     :type => "problem"
+    #     )
     
 
-    end
+    #end
     
 
     
