b90546398b552085d16a122873868a07a719609c
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-20 16:34:31 -0500

    Lead mail sent but image not working. Zendesk ticket for lead stopped working

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -14,53 +14,35 @@ class Lead < ApplicationRecord
     validates :message, presence: true, allow_blank: false
 
     mount_uploader :attachment, AttachmentUploader
-    after_create :rocketMail
+    after_create :lead_mail
 end
 
 
-def rocketMail
 
-    @full_name = "#{self.full_name}"
-    @project_name = "#{self.project_name}"
-    @email = "#{self.email}"
+def lead_mail
+  @full_name = "#{self.full_name}"
+  @project_name = "#{self.project_name}"
+  @email = "#{self.email}"
 
-    mail = SendGrid::Mail.new
-    mail.from = SendGrid::Email.new(email: ENV["SENDGRID_FROM_EMAIL"]) #changed
-    mail.subject = 'Thank you for contacting Rocket Elevators'
-    pp mail
-    personalization = SendGrid::Personalization.new #changed
-    personalization.add_to(SendGrid::Email.new(email: @email))
-    personalization.add_dynamic_template_data({
+  from = SendGrid::Email.new(email:  ENV['SENDGRID_FROM_EMAIL'])
+  to = SendGrid::Email.new(email: @email)
+  subject = 'Thank you for contacting Rocket Elevators'
+  content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+  mail = SendGrid::Mail.new(from, subject, to, content)
+
+  personalization = SendGrid::Personalization.new #changed
+  personalization.add_to(SendGrid::Email.new(email: @email))
+  personalization.add_dynamic_template_data({
         "full_name" => @full_name,
         "project_name" => @project_name,
     })
-    mail.add_personalization(personalization)
-    mail.template_id = 'd-4b7f69bd582143e5b550e78e1deb1ab9'
-    mail.send_at = SendGrid::Email.new(email: "jorgemarcoux@gmail.com") #changed
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    
-    pp personalization 
+ mail.add_personalization(personalization)
+ mail.template_id = 'd-4b7f69bd582143e5b550e78e1deb1ab9'
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
- 
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  
 end
-
-##EMAIL WORKING BUT WITHOUT TEMPLATE
-# def test_mail
-#   from = SendGrid::Email.new(email: 'jorgemarcoux@gmail.com')
-#   to = SendGrid::Email.new(email: 'jorgemarcoux@gmail.com')
-#   subject = 'Thank you for contacting Rocket Elevators'
-#   content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-#   mail = SendGrid::Mail.new(from, subject, to, content)
-
-#   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-#   response = sg.client.mail._('send').post(request_body: mail.to_json)
-#   puts response.status_code
-#   puts response.body
-#   puts response.headers
-# end
   
 
 # def dropbox
