c96a7c66dc5f72c125854a7bb97eafa98ad94a97
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-24 09:26:49 -0500

    YT API exemple working

--- lib/quote_api_test.rb
+++ lib/quote_api_test.rb
@@ -1,2 +1,15 @@
 require 'rubygems'
-require'httpparty'
+require'httparty'
+
+class QuoteApi
+    include HTTParty
+    base_uri "edutechional-resty.herokuapp.com/"
+
+    def posts
+      self.class.get('/posts.json')
+    end
+
+end
+
+quote = QuoteApi.new
+puts quote.posts
