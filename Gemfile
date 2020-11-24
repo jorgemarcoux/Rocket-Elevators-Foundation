87bb58a3039967f0b39d6695941f3d96e69dd154
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-24 16:00:40 -0500

    connection test fails thanks to webmock

--- Gemfile
+++ Gemfile
@@ -115,6 +115,7 @@ group :test do
   gem 'chromedriver-helper'
   #TTD Week 10
   gem 'rspec-rails', '~> 4.0.1'
+  gem "webmock"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  #Disabling HTTP external requests for tests
+  require 'webmock/rspec'
+  WebMock.disable_net_connect!(allow_localhost: true)
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
