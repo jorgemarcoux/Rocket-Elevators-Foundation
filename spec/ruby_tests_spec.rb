c7479c229668aa34adda25680e656b0cde26f1a1
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-26 09:23:21 -0500

    avoid_employee method passes test with string

--- spec/ruby_tests_spec.rb
+++ spec/ruby_tests_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 require "rails_helper"
 
 RSpec.describe LeadsController, type: :controller do
-     describe "#lead_params" do
+     describe "#avoid_employee" do
         context 'Avoid CodeBoxx employees to fill out contact form' do
           it 'sends alerts if you use a CodeBoxx email' do
-
+          expect(LeadsController.avoid_employee). to eql("DON'T")
           end
         end
     end
