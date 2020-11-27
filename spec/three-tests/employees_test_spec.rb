77c2d60330b83b3dcc14421b25d09b3fa545a1d8
JORGEMARCOUX <JORGEMARCOUX@GMAIL.COM>
2020-11-26 19:32:46 -0500

    CodeEmployee assignRole works properly

--- spec/three-tests/employees_test_spec.rb
+++ spec/three-tests/employees_test_spec.rb
@@ -4,17 +4,21 @@ require 'spec_helper'
 class CodeBoxxEmployee
     def self.assignRole
         user = User.new
+        if user.email == '""@codeboxx.com'
+          user.is_employee == 1
+        end
         return "employee"
     end
 end
 
 RSpec.describe CodeBoxxEmployee do
- it 'automatically assigns the employee role to users with CodeBoxx emails' do
-     user = User.new
-    expect(CodeBoxxEmployee.assignRole).to eq("employee")
- end
+    describe ".assignRole" do 
+       it 'automatically assigns the employee role to users with CodeBoxx emails' do
+         user = User.new
+         expect(CodeBoxxEmployee.assignRole).to eq("employee")
+       end
+        end
  
-
 end
 
 
